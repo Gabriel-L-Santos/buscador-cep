f2693d90fddad18fe40979d9a46c28abb6f31832
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-02-17 21:07:47 -0300

    edits js e css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 function App() {
   return (
-    <div className="App">
-      <h1>TESTE PROJETO</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu CEP..."
+        />
+      </div>
+
     </div>
   );
 }
